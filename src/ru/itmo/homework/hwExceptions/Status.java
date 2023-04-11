5fc390c46a1313864bd2fe26919a0c0fce23aee1
andrbreus <andrbreus@yandex.ru>
2023-04-11 15:06:29 +0300

    add solution to hw on exceptions

---
+++ src/ru/itmo/homework/hwExceptions/Status.java
@@ -0,0 +1,7 @@
+package ru.itmo.homework.hwExceptions;
+
+public enum Status {
+    FILE_NOT_FOUND,
+    ACCESS_DENIED,
+    JAR_ERROR,
+}
---
+++ src/ru/itmo/homework/hwExceptions/Task.java
@@ -0,0 +1,28 @@
+package ru.itmo.homework.hwExceptions;
+
+import java.io.FileNotFoundException;
+import java.nio.file.AccessDeniedException;
+import java.util.jar.JarException;
+
+public class Task {
+    private static void throwException(Status status)
+            throws JarException, FileNotFoundException, AccessDeniedException {
+        switch (status) {
+            case JAR_ERROR -> throw new JarException();
+            case ACCESS_DENIED -> throw new AccessDeniedException("file");
+            case FILE_NOT_FOUND -> throw new FileNotFoundException();
+        }
+    }
+    public static void main(String[] args) {
+        var status = Status.ACCESS_DENIED;
+
+        try {
+            throwException(status);
+        } catch (FileNotFoundException | JarException e) {
+            e.printStackTrace();
+        } catch (AccessDeniedException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+}
