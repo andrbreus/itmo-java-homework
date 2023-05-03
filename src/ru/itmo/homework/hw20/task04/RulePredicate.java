c0225b5f313ee69cb44b943670a3569a28c5dd94
andrbreus <andrbreus@yandex.ru>
2023-05-03 10:23:44 +0300

    update task04 of hw18

--- src/ru/itmo/homework/hw20/task04/RulePredicate.java
+++ src/ru/itmo/homework/hw20/task04/RulePredicate.java
@@ -3,7 +3,11 @@ package ru.itmo.homework.hw20.task04;
 import java.util.function.Predicate;
 
 public interface RulePredicate extends Predicate<String> {
-    default Predicate<String> andRule(Predicate<String> other) {
-        return Predicate.super.and(other);
+    default RulePredicate andRule(RulePredicate other) {
+        return (t) -> test(t) && other.test(t);
+    }
+
+    default RulePredicate orRule(Predicate<String> other) {
+        return (t) -> test(t) || other.test(t);
     }
 }
--- src/ru/itmo/homework/hw20/task04/Task04.java
+++ src/ru/itmo/homework/hw20/task04/Task04.java
@@ -4,27 +4,16 @@ import java.util.List;
 
 public class Task04 {
     public static void main(String[] args) {
-        RulePredicate rule01 = new RulePredicate() {
-            @Override
-            public boolean test(String s) {
-                return s.length() > 7;
-            }
-        };
+        RulePredicate rule01 = filename -> filename.length() > 7;
+        RulePredicate rule02 = filename -> filename.endsWith(".txt");
+        RulePredicate rule03 = filename -> filename.endsWith(".log");
+        RulePredicate rule04 = filename -> Character.isUpperCase(filename.charAt(0));
 
-        RulePredicate rule02 = new RulePredicate() {
-            @Override
-            public boolean test(String s) {
-                return s.endsWith(".txt");
-            }
-        };
-
-        FileStorage fileStorage =
-                new FileStorage(rule01.andRule(rule02.andRule(filename -> Character.isUpperCase(filename.charAt(0)))));
-
-        List<String> fileNames = List.of("File.txt", "file.txt", "Song.mp3", "QWERTY.txt", "F.txt");
+        FileStorage storage = new FileStorage(rule01.andRule(rule02.orRule(rule03)).andRule(rule04));
+        List<String> fileNames = List.of("File.txt", "file.txt", "Song.mp3", "QWERTY.txt", "F.txt", "Log001.log");
 
         for (String fileName : fileNames) {
-            if (fileStorage.add(fileName)) System.out.println(fileName + " added!");
+            if (storage.add(fileName)) System.out.println(fileName + " added!");
         }
     }
 }
